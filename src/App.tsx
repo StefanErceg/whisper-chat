c23ac103b5faa31fa8dd8ae333b82657316cf1d1
Stefan Erceg <stefanerceg95@gmail.com>
2023-02-20 20:10:28 +0100

    initialize React project with vite and TailwindCSS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+	return (
+		<div className="h-screen w-screen">
+			<div className="w-full h-full flex justify-center items-center">
+				<div className="h-20 flex items-center p-4 bg-blue-100 text-slate-900 font-semibold text-lg rounded-xl">
+					<span>Hello from whisper!</span>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
